faf08d4961f393e3555360d60ecd757c921b575e
John Witherspoon <john.witherspoon@sympatico.ca>
2026-05-20 22:49:39 -0400

    phase1: add runtime config and NVS adapter interfaces

--- lib/MatrixClock_Config/src/MatrixClock_Config.cpp
+++ lib/MatrixClock_Config/src/MatrixClock_Config.cpp
@@ -1,6 +1,30 @@
 #include "MatrixClock_Config.h"
 #include "AP_Config_Portal.h"
 
+MatrixClockRuntimeConfig g_matrixClockRuntimeConfig = {
+  MATRIXCLOCK_CONFIG_SCHEMA_VERSION,
+  {}
+};
+
 void matrixClockConfigRegisterPortalContracts() {
   // Phase 0 contract stub only: page/field registration and callbacks are defined later.
+}
+
+bool matrixClockConfigLoadFromNvs(MatrixClockRuntimeConfig &outConfig) {
+  (void)outConfig;
+  return false;
+}
+
+bool matrixClockConfigSaveToNvs(const MatrixClockRuntimeConfig &config) {
+  (void)config;
+  return false;
+}
+
+bool matrixClockConfigIsSchemaCompatible(uint16_t storedVersion) {
+  return storedVersion == MATRIXCLOCK_CONFIG_SCHEMA_VERSION;
+}
+
+void matrixClockConfigLoadBootstrapDefaults(MatrixClockRuntimeConfig &outConfig) {
+  outConfig.schemaVersion = MATRIXCLOCK_CONFIG_SCHEMA_VERSION;
+  outConfig.configDb = configDb;
 }
--- lib/MatrixClock_Config/src/MatrixClock_Config.h
+++ lib/MatrixClock_Config/src/MatrixClock_Config.h
@@ -2,7 +2,22 @@
 #define MATRIXCLOCK_CONFIG_H
 
 #include <Arduino.h>
+#include "globals.h"
+
+static const uint16_t MATRIXCLOCK_CONFIG_SCHEMA_VERSION = 1;
+
+struct MatrixClockRuntimeConfig {
+	uint16_t schemaVersion;
+	configDb_t configDb;
+};
+
+extern MatrixClockRuntimeConfig g_matrixClockRuntimeConfig;
 
 void matrixClockConfigRegisterPortalContracts();
+
+bool matrixClockConfigLoadFromNvs(MatrixClockRuntimeConfig &outConfig);
+bool matrixClockConfigSaveToNvs(const MatrixClockRuntimeConfig &config);
+bool matrixClockConfigIsSchemaCompatible(uint16_t storedVersion);
+void matrixClockConfigLoadBootstrapDefaults(MatrixClockRuntimeConfig &outConfig);
 
 #endif // MATRIXCLOCK_CONFIG_H
